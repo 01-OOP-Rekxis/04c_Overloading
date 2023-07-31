ab48a1436ae72b78f59dd79389d8316abed2ae87
Rekxis <Rekxis@gmx.de>
2023-07-31 12:10:45 +0200

    format and comments

--- com/cc/java/TestOverloading.java
+++ com/cc/java/TestOverloading.java
@@ -12,7 +12,9 @@ public class TestOverloading {
 	// System.out.println("Hello");
 	// }
 
+
 	// Gleiche Signatur | return type ?
+
 	// public void sayHello() {
 	// System.out.println("Hello");
 	// }
@@ -22,7 +24,9 @@ public class TestOverloading {
 	// return "hi";
 	// }
 
+
 	// Gleiche Signatur 3 | Access Modifier ?
+
 	// public void sayHello() {
 	// System.out.println("Hello");
 	// }
@@ -31,7 +35,9 @@ public class TestOverloading {
 	// System.out.println("Hello");
 	// }
 
+
 	// Gleiche Signatur 3 | Parameter ?
+
 	// public void sayHello2(String name, int counter) {
 	// System.out.println("Parameter name: " + name + " | Parameter: counter: " +
 	// counter);
@@ -41,8 +47,10 @@ public class TestOverloading {
 	// System.out.println("Parameter name: " + name + " | Parameter: counter: " +
 	// counter);
 	// }
+
 
 	// Gleiche Signatur 3 | Parameter Namen ?
+
 	// public void sayHello2(String name, int counter) {
 	// System.out.println("Parameter name: " + name + " | Parameter: counter: " +
 	// counter);
@@ -58,22 +66,22 @@ public class TestOverloading {
 		System.out.println("1: Parameter name: " + name + " | Parameter: counter: " + counter);
 	}
 
-	// // Überladene Methode 1 (Reihenfolge Parameter)
+	// Überladene Methode 1 (Reihenfolge Parameter)
 	public void sayHello2(int counter, String name) {
 		System.out.println("2: Parameter name: " + name + " | Parameter: counter: " + counter);
 	}
 
-	// // Überladene Methode 3 (Anzahl Parameter)
+	// Überladene Methode 3 (Anzahl Parameter)
 	public void sayHello2() {
 		System.out.println("3: Ohne parameter");
 	}
 
-	// // BasisMethode
+	// BasisMethode
 	public void sayHello3(String name, int ID) {
 		System.out.println("Parameter name: " + name + " | Parameter: ID: " + ID);
 	}
 
-	// // Überladene Methode 3 (Parameter Typ)
+	// Überladene Methode 3 (Parameter Typ)
 	public void sayHello3(String name, String ID) {
 		System.out.println("Parameter name: " + name + " | Parameter: ID: " + ID);
 	}
